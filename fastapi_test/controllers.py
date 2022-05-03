2b0ebac918a68a1133a36f987b3e73ed1b182001
Takumi71 <84171930+Takumi71@users.noreply.github.com>
2022-05-04 00:48:58 +0900

    Add files via upload

--- fastapi_test/controllers.py
+++ fastapi_test/controllers.py
@@ -11,6 +11,12 @@ from models import User, Task  # new
 
 import hashlib  # new
 
+import re
+
+pattern = re.compile(r'\w{4,20}')  # 任意の4~20の英数字を示す正規表現
+pattern_pw = re.compile(r'\w{6,20}')  # 任意の6~20の英数字を示す正規表現
+pattern_mail = re.compile(r'^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$')  # e-mailの正規表現
+
 
 app = FastAPI(
     title='FastAPIでつくるtoDoアプリケーション',
@@ -53,17 +59,60 @@ def index(request: Request):
                                       {'request': request
                                       })
 
-# def admin(request: Request):
-#     # ユーザとタスクを取得
-#     # とりあえず今はadminユーザのみ取得
-#     user = db.session.query(User).filter(User.username == 'admin').first()
-#     task = db.session.query(Task).filter(Task.user_id == user.id).all()
-#     db.session.close()
- 
-#     return templates.TemplateResponse('admin.html',
-#                                       {'request': request,
-#                                        'user': user,
-#                                        'task': task})
 
 
+async def register(request: Request):
+    if request.method == 'GET':
+        return templates.TemplateResponse('register.html',
+                                          {'request': request,
+                                           'username': '',
+                                           'error': []})
+
+    if request.method == 'POST':
+        # POSTデータ
+        data = await request.form()
+        username = data.get('username')
+        password = data.get('password')
+        password_tmp = data.get('password_tmp')
+        mail = data.get('mail')
+
+        # new ここから
+        error = []
+
+        tmp_user = db.session.query(User).filter(User.username == username).first()
+
+        # 怒涛のエラー処理
+        if tmp_user is not None:
+            error.append('同じユーザ名のユーザが存在します。')
+        if password != password_tmp:
+            error.append('入力したパスワードが一致しません。')
+        if pattern.match(username) is None:
+            error.append('ユーザ名は4~20文字の半角英数字にしてください。')
+        if pattern_pw.match(password) is None:
+            error.append('パスワードは6~20文字の半角英数字にしてください。')
+        if pattern_mail.match(mail) is None:
+            error.append('正しくメールアドレスを入力してください。')
+
+        # エラーがあれば登録ページへ戻す
+        if error:
+            return templates.TemplateResponse('register.html',
+                                              {'request': request,
+                                               'username': username,
+                                               'error': error})
+
+        # 問題がなければユーザ登録
+        user = User(username, password, mail)
+        db.session.add(user)
+        db.session.commit()
+        db.session.close()
+
+        return templates.TemplateResponse('complete.html',
+                                          {'request': request,
+                                           'username': username})
+    
+    # if request.method is 'GET':
+    #     return templates.TemplateResponse('register.html',
+    #                                       {'request': request,
+    #                                        'username': '',
+    #                                        'error': []})
 
